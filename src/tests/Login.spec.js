f756cd969bcc87b6297ed6644224e971a1bdbc32
victor gabriel <gabrielvghs666@gmail.com>
2022-05-25 08:01:21 -0300

    test: implementação de novos testes

--- src/tests/Login.spec.js
+++ src/tests/Login.spec.js
@@ -2,9 +2,17 @@ import { screen } from '@testing-library/react'
 import React from 'react'
 import { renderWithRouterAndRedux } from '../helpers/renderWithRouterAndRedux'
 import App from '../App'
+import logo from '../trivia.png';
 import userEvent from '@testing-library/user-event'
 
 describe('testa se a página de login contém os campos necessários', () => {
+  test('testa se existe uma imagem na página', () => {
+    renderWithRouterAndRedux(<App />)
+    const imgEl = screen.getByRole('img', {name: 'logo'});
+
+    expect(imgEl).toBeInTheDocument()
+    expect(imgEl).toHaveAttribute('src', logo);
+  })
   test('testa se a página possui dois inputs para o usuário', () => {
     renderWithRouterAndRedux(<App />)
     const InputEl = screen.getAllByRole('textbox');
@@ -13,12 +21,23 @@ describe('testa se a página de login contém os campos necessários', () => {
     InputEl.map((input) => expect(input).toBeInTheDocument());
   })
 
-  test('testa se a página possui um botão', () => {
-    renderWithRouterAndRedux(<App />);
+  test('verifica se os inputs possuem os data-testid correspondentes', () => {
+    renderWithRouterAndRedux(<App />)
+    const InputEl = screen.getAllByRole('textbox');
+    const dataTestIdName = "input-player-name";
+    const dataTestIdEmail = "input-gravatar-email"
 
+    expect(InputEl[0]).toHaveAttribute('data-testid', dataTestIdName)
+    expect(InputEl[1]).toHaveAttribute('data-testid', dataTestIdEmail)
+  })
+
+  test('testa se a página possui um botão com um data-testid', () => {
+    renderWithRouterAndRedux(<App />);
+    const dataTestIdButton = "btn-play"
     const buttonEl = screen.getByRole('button', { name: 'Play' });
 
     expect(buttonEl).toBeInTheDocument();
+    expect(buttonEl).toHaveAttribute('data-testid', dataTestIdButton)
   })
 
   test('testa se ao digitar um usuário inválido, o botão está desabilitado', () => {
@@ -56,7 +75,30 @@ describe('testa se a página de login contém os campos necessários', () => {
     userEvent.type(input[1], userEmail);
 
     userEvent.click(buttonEl);
-    expect(history.location.pathname).toEqual('/Game');
 
+    setInterval(() => {
+      expect(history.location.pathname).toEqual('/Game');
+    }, 2000);
+
+  })
+
+  test('testa se possui um botão com o texto Settings', () => {
+    renderWithRouterAndRedux(<App />);
+    const buttonEl = screen.getByRole('button', { name: 'Settings' });
+
+    expect(buttonEl).toBeInTheDocument();
+  })
+  test('testa se ao clicar no botão, ele redireciona para página Settings', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    const userName = 'pedro'
+    const userEmail = 'pedro@gmail.com'
+    const buttonEl = screen.getByRole('button', { name: 'Settings' });
+    const input = screen.getAllByRole('textbox');
+
+    userEvent.type(input[0], userName);
+    userEvent.type(input[1], userEmail);
+    userEvent.click(buttonEl);
+
+    expect(history.location.pathname).toEqual('/Settings')
   })
 })
