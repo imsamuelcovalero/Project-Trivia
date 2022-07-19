1926e1b2f6ab2bf57afcf8ab4286eaa9490c8a4d
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2022-07-19 11:13:53 -0300

    Fix: Muda a forma de exportação do saveRanking por conta do deploy no Vercel

--- src/helpers/saveRanking.js
+++ src/helpers/saveRanking.js
@@ -1,6 +1,6 @@
 const getRanking = require('./getRanking');
 
-const saveRanking = (ranking) => {
+export const saveRanking = (ranking) => {
   if (getRanking() === null) {
     localStorage.setItem('ranking', JSON.stringify([ranking]));
   } else {
@@ -13,6 +13,3 @@ const saveRanking = (ranking) => {
     );
   }
 };
-if (typeof module !== 'undefined') {
-  module.exports = saveRanking;
-}
--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import saveRanking from '../helpers/saveRanking';
+import { saveRanking } from '../helpers/saveRanking';
 import Header from '../components/Header';
 import cryCat from '../cryCat.jpeg';
 import catLaugh from '../catLaugh.gif';
